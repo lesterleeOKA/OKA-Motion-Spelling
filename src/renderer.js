97a81b1d5cb09971a725934e40dfc486a41f32f4
LAPTOP-V3JB56OU\lesterleeOKA <lesterleeopenknowledgehk@gmail.com>
2024-08-13 14:59:47 +0800

    updated font type and check case of redbox area

--- src/renderer.js
+++ src/renderer.js
@@ -91,8 +91,9 @@ export class RendererCanvas2d {
       //nose, left_eye_inner, left_eye, left_eye_outer, right_eye_inner, right_eye, right_eye_outer, left_ear, right_ear, mouth_left, mouth_right, left_shoulder, right_shoulder, left_elbow, right_elbow, left_wrist, right_wrist, left_pinky, right_pinky, left_index, right_index, left_thumb, right_thumb, left_hip, right_hip, left_knee, right_knee, left_ankle, right_ankle, left_heel, right_heel, left_foot_index, right_foot_index
       //let checkKeypoints = pose.keypoints.filter(k=>['left_shoulder', 'right_shoulder', 'left_elbow', 'right_elbow', 'left_wrist', 'right_wrist', 'left_hip', 'right_hip', 'left_knee', 'right_knee'].includes(k.name) && k.score>0.65);
 
-      if (this.center_shoulder) {
-        isBodyOutBox = ((this.center_shoulder.x < this.redBoxX ||
+      /*if (this.center_shoulder) {
+        isBodyOutBox = ((
+          this.center_shoulder.x < this.redBoxX ||
           this.center_shoulder.x > (this.redBoxX + this.redBoxWidth) ||
           this.center_shoulder.y < this.redBoxX ||
           this.center_shoulder.y > (this.redBoxY + this.redBoxHeight)) ? true : false);
@@ -101,7 +102,7 @@ export class RendererCanvas2d {
         isBodyOutBox = false;
       }
 
-      /*let checkKeypoints = pose.keypoints.filter(k => k.name == 'nose' && k.score > passScore);
+      let checkKeypoints = pose.keypoints.filter(k => k.name == 'nose' && k.score > passScore);
       let isBodyOutBox = (
         checkKeypoints.find(keypoint => (
           keypoint.x < this.redBoxX ||
@@ -110,6 +111,38 @@ export class RendererCanvas2d {
           keypoint.y > (this.redBoxY + this.redBoxHeight)
         )) ? true : false
       );*/
+
+      if (this.center_shoulder) {
+        const isShoulderOutBox = (
+          this.center_shoulder.x < this.redBoxX ||
+          this.center_shoulder.x > (this.redBoxX + this.redBoxWidth) ||
+          this.center_shoulder.y < this.redBoxY ||
+          this.center_shoulder.y > (this.redBoxY + this.redBoxHeight)
+        );
+
+        const isNoseOutBox = pose.keypoints
+          .filter(k => k.name === 'nose' && k.score > passScore)
+          .some(keypoint =>
+            keypoint.x < this.redBoxX ||
+            keypoint.x > (this.redBoxX + this.redBoxWidth) ||
+            keypoint.y < this.redBoxY ||
+            keypoint.y > (this.redBoxY + this.redBoxHeight)
+          );
+
+        isBodyOutBox = isShoulderOutBox && isNoseOutBox;
+      } else {
+        const isNoseOutBox = pose.keypoints
+          .filter(k => k.name === 'nose' && k.score > passScore)
+          .some(keypoint =>
+            keypoint.x < this.redBoxX ||
+            keypoint.x > (this.redBoxX + this.redBoxWidth) ||
+            keypoint.y < this.redBoxY ||
+            keypoint.y > (this.redBoxY + this.redBoxHeight)
+          );
+
+        isBodyOutBox = isNoseOutBox;
+      }
+
       State.setPoseState('bodyInsideRedBox', !isBodyOutBox);
       if (isBodyOutBox) {
         if (State.state == 'playing') State.changeState('outBox', 'outBox');
