91ad3d60ec2a155f160cf27314c22ba261440e72
LAPTOP-V3JB56OU\lesterleeOKA <lesterleeopenknowledgehk@gmail.com>
2024-11-21 15:00:14 +0800

    added random sort to set new question

--- src/spelling.js
+++ src/spelling.js
@@ -280,7 +280,7 @@ export default {
         if (!this.finishedCreateOptions && this.randomPair.length > 0) {
           if (this.fallingItems.length < this.randomPair.length) {
             if (this.fallingId < this.fallingItems.length) {
-              logController.log("falling id:", this.fallingId);
+              //logController.log("falling id:", this.fallingId);
               this.fallingId += 1;
             } else {
               this.fallingId = 0;
@@ -962,6 +962,7 @@ export default {
     this.refallingId = 0;
   },
   moveToNextQuestion() {
+    View.preloadedFallingImages.sort(() => Math.random() - 0.5);
     this.randomQuestion = null;
     this.randomPair.splice(0);
     View.stageImg.innerHTML = '';
